bc4e33a77053b1f79be9c4e8816dcedba71c67c7
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-26 20:49:24 +0200

    add array

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const biciclette = {
-    
-}
+const biciclette = [
+   
+    {nome:"flash", peso: 20 },
+    {nome:"light", peso: 22 },
+    {nome:"speed", peso: 25 },
+    {nome:"crash", peso: 28 }
+]
+
+const pesoMinore = (Math.min(peso))
+
+console.log(pesoMinore)
