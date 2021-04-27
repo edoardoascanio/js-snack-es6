7afd893f6ff571b29a424dc3974e3462f2ba2de6
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-27 10:25:45 +0200

    add ciclo for

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const biciclette = [
-   
-    {nome:"flash", peso: 20 },
-    {nome:"light", peso: 22 },
-    {nome:"speed", peso: 25 },
-    {nome:"crash", peso: 28 }
-]
 
-const pesoMinore = Math.min({peso} = biciclette)
+    { nome: "flash", peso: 10 },
+    { nome: "light", peso: 12 },
+    { nome: "speed", peso: 15 },
+    { nome: "crash", peso: 18 },
+];
 
-console.log(pesoMinore);
+
+
+const trovaPesoMinore = (listaArray) => {
+    let pesoMinore;
+
+    for (let i = 0; i < biciclette.length; i++) {
+        const { nome, peso } = listaArray[i];
+
+
+        if (!pesoMinore || peso < pesoMinore.peso) {
+            pesoMinore = { nome, peso }
+        }
+    }
+
+    return pesoMinore;
+}
+
+const pesoMinoreTrovato = trovaPesoMinore(biciclette);
+console.log(pesoMinoreTrovato)
